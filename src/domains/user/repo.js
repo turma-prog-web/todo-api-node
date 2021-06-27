46574480d557ab9814876221a4a905e8509519eb
Carlos Alberto <carloshtcursos@gmail.com>
2021-06-27 12:19:27 -0400

    task(): update user schema

--- src/domains/user/repo.js
+++ src/domains/user/repo.js
@@ -4,6 +4,9 @@ const modelName = 'User';
 const modelObject = {
   name: { type: String, required: true, default: null },
   email: { type: String, required: true, default: null, unique: true },
+  picture: { type: String, required: true, default: null },
+  code: { type: String, required: true, default: null },
+  active: { type: Boolean, required: true, default: true },
 };
 const schemaDocument = new Schema(modelObject, { timestamps: true });
 
